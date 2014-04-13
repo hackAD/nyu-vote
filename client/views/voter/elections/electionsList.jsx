e52988d77721dac3c81a739e4275adc04e766904
Koh Terai <rootrootrootrootrootroot@gmail.com>
2014-04-13 21:45:01 +0400

    Ballot list CSSed with Media Queries.

--- client/views/voter/elections/electionsList.jsx
+++ client/views/voter/elections/electionsList.jsx
@@ -20,22 +20,26 @@ ElectionsList = ReactMeteor.createClass({
   render: function() {
     return(
       <div id="election-list">
-        <div>
+        <div className="white-bg header">
           Welcome {Meteor.user().profile.name}
         </div>
         <div className="light-green-bg">
-          <div>
-            <h2>You have</h2>
-            <h1>{this.state.openElectionsCount}</h1>
-            <h2>Open Ballots</h2>
+          <div className="ballot-list">
+            <div id="ballot-counter">
+              <h3>You have</h3>
+              <h1>{this.state.openElectionsCount}</h1>
+              <h3>Open Ballots</h3>
+            </div>
             {this.state.openElectionNodes}
           </div>
         </div>
-        <div>
-          <h2>Closed Ballots</h2>
-          {this.state.closedElectionNodes}
+        <div className="dark-blue-bg">
+          <div className="ballot-list">
+            <h2>Closed Ballots</h2>
+            {this.state.closedElectionNodes}
+          </div>
         </div>
-        <a href="#" onClick={this.logout}>Logout</a>
+        <a id="login-button" href="#" onClick={this.logout}>Logout</a>
       </div>
     );
   }
