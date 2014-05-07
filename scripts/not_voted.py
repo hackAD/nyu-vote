95b2aabab3ebc08f8afed987007ee78ba0bd78bd
lingz <lz781@nyu.edu>
2014-05-07 11:59:37 +0400

    added mongo extraction script

---
+++ scripts/not_voted.py
@@ -0,0 +1,27 @@
+import pymongo
+import re
+
+def main():
+  with open("mongo_keys.txt") as f
+    mongo_uri = f.read()
+  client = pymongo.MongoClient(mongo_uri)
+  db = client["nyu-vote"]
+  netIds = set()
+  for group in db.groups.find():
+    netIds = netIds.union(set(group["netIds"]))
+  for ballot in db.ballots.find():
+    netIds.discard(ballot["netId"])
+  pattern = re.compile("^[a-zA-Z]{1,6}[0-9]{1,4}$")
+  actual_count = 0
+  count = 0
+  for netId in netIds:
+    if pattern.match(netId):
+      count += 1
+      print(netId + "@nyu.edu,")
+  print(count)
+
+
+
+
+if __name__ == "__main__":
+  main()
---
+++ scripts/not_voted_single.py
@@ -0,0 +1,28 @@
+import pymongo
+import re
+
+def main():
+  with open("mongo_keys.txt") as f
+    mongo_uri = f.read()
+  client = pymongo.MongoClient(mongo_uri)
+  db = client["nyu-vote"]
+  netIds = set()
+  group = db.groups.find_one({"slug": "student-government-elections"})
+  netIds = netIds.union(set(group["netIds"]))
+  election = db.elections.find_one({"slug": "student-government-election-"})
+  for ballot in db.ballots.find({"electionId": election["_id"]}):
+    netIds.discard(ballot["netId"])
+  pattern = re.compile("^[a-zA-Z]{1,6}[0-9]{1,4}$")
+  actual_count = 0
+  count = 0
+  for netId in netIds:
+    if pattern.match(netId):
+      count += 1
+      print(netId + "@nyu.edu,")
+  print(count)
+
+
+
+
+if __name__ == "__main__":
+  main()
