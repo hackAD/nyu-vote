1ebac2cf8343d476ad7438781cfd09f4461114e8
Lingliang Zhang <lingliangz@gmail.com>
2014-08-30 17:20:14 -0400

    Working for docker

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,14 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ENV_ARGS=""
 
-if [ -e DIR/../config ]; then
+if [ -e $DIR/../config ]; then
   # Read environment args from config file
   while read line; do
     ENV_ARGS="$ENV_ARGS -e $line"
-  done < DIR/../config
+  done < $DIR/../config
 
   # Strip the first whitespace away
   ENV_ARGS=${ENV_ARGS:1}
 fi
 
-docker run -i -t --rm --name $ENV_ARGS nyu-vote -p 3000:3000 lingz/nyu-vote
+docker run $ENV_ARGS --rm --name nyu-vote -p 3000:3000 lingz/nyu-vote
